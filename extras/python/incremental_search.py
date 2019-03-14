61cb12af8f348c27c64ba87afd7e64261ccde388
evinracher <eevinley@gmail.com>
2019-03-13 23:12:09 -0500

    Adding incremental search and organizating folders

---
+++ extras/python/incremental_search.py
@@ -0,0 +1,32 @@
+# exp() and
+import math
+# Function definition: e(3x-12) + x * cos(3*x) - x2 + 4
+def fun(x):
+ return math.exp(3*x - 12) + x * math.cos(3*x) - pow(x, 2) + 4
+
+xi = input("enter the initial value: ")
+delta = input("enter the delta value: ")
+m_iter = input("enter the iterations number: ")
+
+fxi = fun(xi)
+if fxi == 1:
+ print str(xi)+str(" is root")
+else:
+ cont = 1
+ xs = xi + delta
+ fxs = fun(xs)
+ print "x   y"
+ print str(xi) + " " + str(fxi) 
+ while fxi*fxs > 0 and cont < m_iter:
+  xi = xs
+  fxi = fxs
+  xs = xi + delta
+  fxs = fun(xs)
+  cont = cont + 1
+  print str(xs) + " " + str(fxs) 
+ if fxs == 0:
+  print str(xs) + " is a root"
+ elif fxi*fxs < 1:
+  print str("there is a root in ["+ str(xi) +","+ str(xs) +"]")
+ else:
+  print str("failed in "+ str(cont) +" iterations")
