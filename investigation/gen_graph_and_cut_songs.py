ea9783ae348fea62c43dfb986917b813ae1ee5b8
evinracher <eevinley@gmail.com>
2019-04-20 22:08:31 -0500

    On the third day, the Lord was resurrected... I hope our project too. Missing translate all code. I got better interpolation methods but I still think that isn't a good result. I got better the code and the graphs, as I promised

--- investigation/gen_graph_and_cut_songs.py
+++ investigation/gen_graph_and_cut_songs.py
@@ -1,17 +1,17 @@
 """
-In this document, I am going to use the JJ's code to generate the dict, graph it
-and create a cut method that randomly delete part of the dict to simulate
-lost of characteristics in a song.
+This script reads a song in midi format, generate a list of points that 
+represent the song, plot it and modify the list by deleting some points
+simulating lost of characteristics in a song. Then tries to recover the
+some using interpolation and plot the result.
 
+Created on Apr 2019
 
+@autor: Lucumi's Followers
+TODO: Translate to English all code
 """
 
-
 import sys
-import mido
-import audiolazy
 from mido import MidiFile
-from audiolazy import lazy_midi
 from audiolazy.lazy_midi import midi2freq
 import matplotlib.pyplot as plt
 import random
@@ -101,31 +101,38 @@ def graph_arrays(X,Y, title):
     fig.set_ylabel("Frecuency")
     fig.legend(loc='upper center', fontsize='x-large')
 
-def cut_song(n):
+def cut_song(n, random_type):
     """Modify the original song deleting points of it and save the result
     in two global arrays X_array and Y_array.
     
     Parameters
     ----------
-    n: int
+    n : integer
         The numbers of point to be deleted
+    random : bool
+        Is True if you want generated a random list of X values
     
     Returns
     -------
-    random_indexes : list
+    indexes : list
         indexes of points that was deleted
     """
-    
-    random_indexes = random.sample(range(0,len(times)-1),n)
     global X_array
     global Y_array 
-    print(random_indexes)
-    print(len(times))
+    indexes =[]
+    
+    if random_type == False:
+        indexes = [2, 11, 3, 23, 13, 5, 14, 16, 18, 17]
+    else:
+        indexes = random.sample(range(0,len(times)-1),n)
+    print("Deleting the values in these indixes: ",indexes)   
+    indexes.sort()
+ 
     for i in range(len(times)):
-        if not(i in random_indexes):
+        if not(i in indexes):
             X_array.append(times[i])
             Y_array.append(frecuencies[i])
-    return random_indexes
+    return indexes
 
 def L(X, xk, x):
     result = 1.0
@@ -134,21 +141,66 @@ def L(X, xk, x):
             result *= (x - xi)/(xk - xi)
     return result
 
-def interpolate(X,Y, x):
+def interpolate_by_newton(X,Y, x):
     y_evaluation = 0.0
     n = len(X)
     for xk in range(n):
         y_evaluation += Y[xk]*L(X, X[xk], x)
     return y_evaluation
 
-def repair_song(X):
+def interpolate_by_neville(X,Px,x):
+    n = len(X)
+    matriz = [None] * n
+    for i in range(n):
+        matriz[i] = [None] * n
+    for i in range(n):
+        matriz[0][i] = Px[i]
+
+    for grado in range(1,n):
+        j = grado
+        for i in range(0, n-grado):
+            Pi = matriz[grado-1][j-1]
+            Pj = matriz[grado-1][j]
+            matriz[grado][j] = ((x-X[i])*Pj-(x-X[j])*Pi)/(X[j]-X[i])
+            j += 1
+    return matriz[n-1][n-1]
+
+def repair_song_interpolating(X, interpolation_type):
+    """Get the repaired song and save it into X_repaired and Y_repaired lists
+    
+    This method can use two types of interpolations for generated the repaired
+    song: newton and neville
+    
+    Parameters
+    ----------
+    X : list
+        A list of indexes of times list that will be the x values for each
+        interpolation
+    interpolation_type : str
+        A string with the name of the interpolation. It can take either "newton"
+        or "neville" as value
+    
+    Raises
+    ------
+    ValueError
+        if interpolation type isn't an implemented interpolation method    
+    """
     global X_repaired
     X_repaired = X_array.copy()
     global Y_repaired
     Y_repaired = Y_array.copy()
     for i in X:
-        Y_repaired.append(interpolate(X_repaired,Y_repaired, times[i]))
+        if(interpolation_type == "newton"):
+            Y_repaired.append(interpolate_by_newton(X_repaired,Y_repaired, times[i]))
+        elif(interpolation_type== "neville"):
+            Y_repaired.append(interpolate_by_neville(X_array,Y_array, times[i]))
+        else:
+            Ex = ValueError()
+            Ex.strerror = str("Interpolation method unknown: " + interpolation_type)
+            raise Ex
         X_repaired.append(times[i])
+    print("Repairing by interpolation using",interpolation_type, "method")
+    
 
 def order_song(X,Y):
     f_dict ={}
@@ -165,10 +217,8 @@ def main():
     de los archivos midi leídos"""
 
     song = sys.argv[1]
-    print('Transformando canción: ', song)
-    
-    
-
+    print("Transforming song: ", song)
+      
     tiempo_acumulado = 0
     mid = MidiFile(song)
     for i, track in enumerate(mid.tracks):
@@ -188,21 +238,17 @@ def main():
 
     #graph_dict(points)
     graph_arrays(times, frecuencies, "Original song")
-    x = cut_song(10)
+    x = cut_song(10, False)
     graph_arrays(X_array,Y_array, "Modified song")
     plt.show()
-    # MALO de acá en adelante :"V
-    repair_song(x)
-    graph_arrays(times, frecuencies, "Original song")
-    print(times)
-    print(frecuencies)
-    print(X_repaired)
-    print(Y_repaired)
-    order_song(X_repaired, Y_repaired)
-    print(X_repaired)
-    print(Y_repaired)
-    graph_arrays(X_repaired, Y_repaired, "Repaired song")
-    plt.show()
+    try:
+        repair_song_interpolating(x, "neville")
+        graph_arrays(times, frecuencies, "Original song")
+        order_song(X_repaired, Y_repaired)
+        graph_arrays(X_repaired, Y_repaired, "Repaired song")
+        plt.show()
+    except ValueError as e:
+        print("ValueError Exception! ", e.strerror)
     return points
     
 if __name__ == '__main__':
