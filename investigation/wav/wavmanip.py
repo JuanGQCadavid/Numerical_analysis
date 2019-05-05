33fbc9246c10a31cb6a621c2a282813adf320a3e
Agustín Nieto García <anietog1@eafit.edu.co>
2019-05-04 22:33:40 -0500

    Changing interface. Points -> x = [], y = []. Wav -> rate = int, samples = np.array

--- investigation/wav/wavmanip.py
+++ investigation/wav/wavmanip.py
@@ -1,9 +1,13 @@
 from scipy.io import wavfile
-from numpy import arange
+import numpy
 
 def wav2points(sample_rate, samples):
     samplespersecond = 1.0 / sample_rate
-    return arange(0.0, samplespersecond * len(samples), samplespersecond), samples
+    duration = samplespersecond * len(samples)
+    x = numpy.arange(0.0, duration, samplespersecond)
+    return x.tolist(), samples.tolist()
 
 def points2wav(x, y):
-    return int(1.0 / (x[1] - x[0])), y
+    sample_rate = int(1.0 / (x[1] - x[0]))
+    samples = numpy.array(y, dtype=numpy.int16)
+    return sample_rate, samples
