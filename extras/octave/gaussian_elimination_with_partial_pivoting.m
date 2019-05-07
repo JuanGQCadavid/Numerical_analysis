e1f34b9dc62748e3cdf7d2b85150d26e19edf683
evinracher <eevinley@gmail.com>
2019-05-07 10:59:10 -0500

    Another one

--- extras/octave/gaussian_elimination_with_partial_pivoting.m
+++ extras/octave/gaussian_elimination_with_partial_pivoting.m
@@ -1,3 +1,4 @@
+digits = 10;
 output_precision(7)
 #Correct decimals with round
 A = double([
@@ -7,8 +8,8 @@ A = double([
 -12 13 -8 -4 -32]);
 n = size(A)(1);
 
-A_aux = double(A)
-function retvar = simple_gaussian_elimination(A,n)
+A_aux = double(A);
+function retvar = gaussian_elimination_partial_pivoting(A,n)
   for k = 1:n-1
     disp(cstrcat("Etapa: ", mat2str(k)))
     A
@@ -26,22 +27,28 @@ function retvar = simple_gaussian_elimination(A,n)
 endfunction
 
 function pivoted = partial_pivoting(A, k,n)
+  pivoted = A;
   disp("Partial pivoting");
   greater = abs(A(k,k));
   greater_row = k;
   for i = k + 1:n
     if abs(A(i,k)) > greater
-      greater = abs(A(i,k))
-      greater_row = i
+      greater = abs(A(i,k));
+      greater_row = i;
     endif
   endfor
   if greater == 0
     error("Equations system hasn't solution");
   endif
-  pivoted = change_rows(A, k, greater_row);
+  if greater_row != k
+    pivoted = change_rows(A, k, greater_row);
+  else
+    disp("Nothing");
+  endif
 endfunction
 
 function changed_matrix = change_rows(A, k, greater_row)
+  disp(cstrcat("Changing row ",mat2str(k), " with ",mat2str(greater_row)));
   aux = A(k,:);
   A(k,:) = A(greater_row,:);
   A(greater_row, :) = aux;
@@ -49,19 +56,20 @@ function changed_matrix = change_rows(A, k, greater_row)
 endfunction
 
 function regressive_substitution(A,n)
+  global digits
   x = [];
   x(n) = A(n,n+1)/A(n,n);
-  disp(cstrcat("X",mat2str(n),": ",mat2str(x(n),10)))
+  disp(cstrcat("X",mat2str(n),": ",mat2str(x(n),digits)))
   for i = n-1:-1:1
     acum = 0;
     for p = i+1:n
       acum = acum + A(i,p)*x(p);
     endfor
     x(i) = (A(i,n+1)-acum)/A(i,i);
-    disp(cstrcat("X",mat2str(i),": ",mat2str(x(i),10)))
+    disp(cstrcat("X",mat2str(i),": ",mat2str(x(i),digits)))
   endfor
 endfunction
 
 
-T = double(simple_gaussian_elimination(A,n))
+T = double(gaussian_elimination_partial_pivoting(A,n))
 regressive_substitution(T,n)
