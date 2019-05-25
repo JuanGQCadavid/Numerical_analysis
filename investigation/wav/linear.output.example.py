3c96e589180ce76ce95fdf6f6ed73b0df6f62e87
Agustín Nieto García <anietog1@eafit.edu.co>
2019-05-25 17:38:11 -0500

    Removing a useless file after changing linear interpolation example to the use of the library.

--- investigation/wav/linear.output.example.py
+++ investigation/wav/linear.output.example.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 from scipy.io import wavfile
-import damage, recognize, repair
+from scipy.interpolate import interp1d
+import damage, recognize, utils
 
 sample_rate, samples = wavfile.read('songs/hakuna_matata.wav')
 
@@ -9,8 +10,12 @@ newsamples = samples.copy()
 damage.zerofill(newsamples, 0.5)
 wavfile.write('songs/zerofill_hakuna_matata.wav', sample_rate, newsamples)
 
-# matches = recognize.kevin(newsamples, max(newsamples) * 0.3)
 matches = recognize.cheat(samples, newsamples)
+validx, validy = utils.tovalidxy(newsamples, matches)
+f = interp1d(validx, validy, fill_value='extrapolate')
 
-repair.linearinterpolation(newsamples, matches)
-wavfile.write('songs/zerofill_cheat_linearinterpolation_hakuna_matata.wav', sample_rate, newsamples)
+invalidx = utils.invalidx(matches)
+fixedy = f(invalidx)
+
+utils.replace(newsamples, invalidx, fixedy)
+wavfile.write('songs/zerofill_cheat_linear_hakuna_matata.wav', sample_rate, newsamples)
