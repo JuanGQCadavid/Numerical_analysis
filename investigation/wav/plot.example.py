ab561ad27c5c62dc6c8eb5188f7ceb7221156bea
Agustín Nieto García <anietog1@eafit.edu.co>
2019-05-25 18:10:37 -0500

    Fixing an issue.

--- investigation/wav/plot.example.py
+++ investigation/wav/plot.example.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-import damage, repair
+import damage
 
 sample_rate, samples = wavfile.read('songs/hakuna_matata.wav')
 samples = samples[1000000:1000500]
@@ -10,13 +10,8 @@ samples = samples[1000000:1000500]
 newsamples = samples.copy()
 damage.zerofill(newsamples, 0.5)
 
-matches = [y0 == y1 for (y0, y1) in zip(samples, newsamples)]
-fixedsamples = newsamples.copy()
-repair.linearinterpolation(fixedsamples, matches)
-
 plt.xlabel('Frame')
 plt.ylabel('Frequency [Hz]')
 plt.plot(samples)
 plt.plot(newsamples)
-plt.plot(fixedsamples)
 plt.show()
