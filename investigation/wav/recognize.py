92ee24f681c9cc4feb0fec67d3d4844dfb17487f
Agustín Nieto García <anietog1@eafit.edu.co>
2019-05-26 17:36:51 -0500

    Adding false positives possibility to the recognition method.

--- investigation/wav/recognize.py
+++ investigation/wav/recognize.py
@@ -1,8 +1,21 @@
-def cheat(y0, y1):
+import random
+
+def cheat(y0, y1, false_positives=0, false_negatives=0):
     """
     Returns an array of True if y0[i] == y1[i], False otherwise.
+    It adds false positives and false negatives in the given "percent".
     """
-    return [i == j for (i, j) in zip(y0, y1)]
+    matches = [i == j for (i, j) in zip(y0, y1)]
+
+    for i in range(len(matches)):
+        if matches[i]:
+            if random.random() < false_negatives:
+                matches[i] = False
+        else:
+            if random.random() < false_positives:
+                matches[i] = True
+
+    return matches
 
 def kevin(y, maxdelta):
     """
