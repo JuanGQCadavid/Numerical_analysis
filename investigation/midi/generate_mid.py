d45a914e447ffeea2d0b3383c2a746ca56249bee
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-05-27 15:05:23 -0500

    Update generate_mid.py

--- investigation/midi/generate_mid.py
+++ investigation/midi/generate_mid.py
@@ -30,3 +30,45 @@ for x in datos:
     track.append(Message('note_off', note=int(nota), velocity=100, time=10))
     tiempo_anterior = tiempo_absoluto
 outfile.save('resultado.mid')
+
+'''
+import mido
+
+mid -> Midi original.
+my_tracks - > [[meta_msg],[notas_msg]]
+path -> md.file_name, midi_partitures/happy.mid
+def write_midi(mid,my_tracks,path):
+    file = MidiFile(type=mid.type)
+    file.ticks_per_beat = mid.ticks_per_beat
+    for track in my_tracks:
+        track_i = MidiTrack()
+        #for msg in track[2]:
+            #track_i.append(msg)
+        
+        for meta_msg in track[0]:
+            track_i.append(meta_msg)
+        for note in track[1]:
+            track_i.append(note)
+        
+        file.tracks.append(track_i)
+        
+    file.save(path)
+    print('wrote')
+    
+ def read_midi(mid): mid = MidiFile('midi_partitures/happy.mid')
+    my_tracks = []
+    for track in mid.tracks:
+        meta_msg = []
+        notes_msg = []
+        msgs = []
+        for msg in track:
+            msgs.append(msg)
+            if msg.type == 'note_on':
+                notes_msg.append(msg)
+            else:
+                meta_msg.append(msg)
+        
+        my_tracks.append([meta_msg,notes_msg,msgs])
+    
+    return my_tracks
+'''
